80d244c7f52dad2d6a81527f95655af7940144f8
cwl <928064529@qq.com>
2020-04-17 16:29:43 +0800

    publisher

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ Vue.use(ElementUI)//全局使用ElementUI
 
 import "./common/stylus/icon.styl"
 
+import $api from './store/index'
+Vue.prototype.$api = $api
+
+import axios from 'axios'
+
 
 Vue.config.productionTip = false
 
@@ -15,3 +20,16 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+// axios({
+//   url: 'http://httpbin.org/', // httpbin.org/   // 默认是Get请求
+//   method: 'get',
+//   // 专门针对get请求的参数拼接
+//   params: {
+//     type: 'pop',
+//     page: 1
+//   }
+// }).then(res => {
+//   console.log(res);
+// })
+
